6ae8fea7f394b4171a9d5af0a8de4a69113b0656
007riche <61353180+007riche@users.noreply.github.com>
2026-06-19 16:48:18 +0200

    FEAT and FIX(Notion Saas clone): fixed  db migration ts and migration done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,18 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
+
 dotenv.config({ path: '.env' });
 
 if (!process.env.DATABASE_URL) {
     console.log('Cannot find database url');
 }
 
-export default {
-    schema: './src/lib/supabase/schema.ts',
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./lib/supabase/schema.ts",
     out: './migrations',
-    driver: 'pg',
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL || '',
-        url
-    }
-} satisfies Config;
+        url: process.env.DATABASE_URL as string || '',
+    },
+});
+
