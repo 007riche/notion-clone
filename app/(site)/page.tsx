47961201553dd4908d98bb83ee0e9ca93206ff1d
007riche <61353180+007riche@users.noreply.github.com>
2026-06-24 17:55:39 +0200

    FEAT(Notion Saas clone/Landing Page): WIP: added hero section bg image

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+import Banner from "@/public/appBanner.png";
 
 function HomePage() {
     // const classes = " overflow-hidden
@@ -13,16 +16,16 @@ function HomePage() {
     // md:items-center";
 
     return (
-        <section>
-            <div className=" overflow-hidden
-    px-4 
-    sm:px-6 
-    mt-10 
-    sm:flex 
-    sm:flex-col 
-    gap-4 
-    md:justify-center 
-    md:items-center">
+        <>
+            <section className=" overflow-hidden
+                px-4 
+                sm:px-6 
+                mt-10 
+                sm:flex 
+                sm:flex-col 
+                gap-4 
+                md:justify-center 
+                md:items-center">
                 <TitleSection pill="✨ Your Workspace, Perfected"
                     title="All-In-One Collaboration and Productivity platform" />
                 <div className="bg-white
@@ -34,7 +37,7 @@ function HomePage() {
                     to-brand-primaryBlue
                     sm:w-\[300px]">
                     <Button
-                        variant="default"
+                        variant="secondary"
                         className=" w-full
                     rounded-[10px]
                     p-6
@@ -43,8 +46,63 @@ function HomePage() {
                         Get Cypress Free
                     </Button>
                 </div>
-            </div>
-        </section>
+                <div className="md:mt-[-90px]
+                sm:w-full
+                w-[750px]
+                flex
+                justify-center
+                items-center
+                mt-[-40px]
+                relative
+                sm:ml-0
+                ml-[-50px]
+                ">
+                    <Image src={Banner}
+                        alt="Application Banner"
+                        loading="lazy"
+                    />
+                    <div className="bottom-0
+                    top-[50%]
+                    bg-linear-to-t
+                    dark:from-background
+                    left-0
+                    right-0
+                    absolute
+                    z-10">
+                    </div>
+                </div>
+            </section>
+            <section className="relative">
+                <div className="overflow-hidden
+                 flex 
+                 after:content['']
+                 after:dark:from-brand-dark
+                 after:to-transparent
+                 after:from-background
+                 after:bg-gradient-to-l
+                 after:right-0
+                 after:bottom-0
+                 after:top-0
+                 after:w-20
+                 after:z-10
+                 after:absolute
+
+
+                 before:content['']
+                 before:dark:from-brand-dark
+                 before:to-transparent
+                 before:bg-gradient-to-r
+                 before:left-0
+                 before:top-0
+                 before:bottom-0
+                 before:w-20
+                 before:z-10
+                 before:absolute
+                ">
+
+                </div>
+            </section>
+        </>
     );
 }
 
